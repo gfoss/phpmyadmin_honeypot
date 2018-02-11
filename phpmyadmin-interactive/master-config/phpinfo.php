81c2e0bb0e182ccbf8e110f204810b1db4bb069e
Christian Davis <Christian@kindhealth.co>
2018-02-11 10:41:12 -0600

    log access to fake phpinfo

--- phpmyadmin-interactive/master-config/phpinfo.php
+++ phpmyadmin-interactive/master-config/phpinfo.php
@@ -1541,4 +1541,31 @@ about PHP licensing, please contact license@php.net.
 </table><br />
 
 </div></body></html>';
+
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+$ip = getRealIpAddr();
+$date = date ("d/m/Y:H:i:s");
+$page = $_SERVER['SCRIPT_FILENAME'];
+$message = "$date _ $ip _ $page\n"; 
+$File = "../log.txt";
+$Open = fopen($File, "a+"); 
+if ($Open){ 
+    fwrite($Open, "$message"); 
+    fclose ($Open); 
+}
 ?>
